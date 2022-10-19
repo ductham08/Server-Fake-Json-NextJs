5f65ed291e45bb8874a27403a5847e095e80bbc2
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-10-20 01:37:30 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ const rules = auth.rewriter({
     // messages: 640
 });
 
+const corsOptions = {
+    origin: 'https://bee-navy.vercel.app',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
 // You must apply the middlewares in the following order
 app.use(cors());
 server.use(rules);
@@ -24,12 +31,17 @@ server.use(middlewares);
 server.use((req, res, next) => {
 
     /*Le Trong Dat*/
-    if (req.method === "POST") {
-        req.body.createAt = Date.now();
-        req.body.updateAt = Date.now();
-    } else if (req.method === "PATCH") {
-        req.body.updateAt = Date.now();
-    }
+    // if (req.method === "POST") {
+    //     req.body.createAt = Date.now();
+    //     req.body.updateAt = Date.now();
+    // } else if (req.method === "PATCH") {
+    //     req.body.updateAt = Date.now();
+    // }
+
+    // Code new
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization ");
     next();
 });
 

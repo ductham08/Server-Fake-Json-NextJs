dde7de9306a0de33c067774e1394e55b14aa7bb8
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-10-20 01:44:00 +0700

    reset Api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const path = require("path");
-var cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults({
@@ -15,15 +14,7 @@ const rules = auth.rewriter({
     // messages: 640
 });
 
-const corsOptions = {
-    origin: 'https://bee-navy.vercel.app',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-
 // You must apply the middlewares in the following order
-app.use(cors());
 server.use(rules);
 server.use(auth);
 
@@ -31,17 +22,12 @@ server.use(middlewares);
 server.use((req, res, next) => {
 
     /*Le Trong Dat*/
-    // if (req.method === "POST") {
-    //     req.body.createAt = Date.now();
-    //     req.body.updateAt = Date.now();
-    // } else if (req.method === "PATCH") {
-    //     req.body.updateAt = Date.now();
-    // }
-
-    // Code new
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization ");
+    if (req.method === "POST") {
+        req.body.createAt = Date.now();
+        req.body.updateAt = Date.now();
+    } else if (req.method === "PATCH") {
+        req.body.updateAt = Date.now();
+    }
     next();
 });
 

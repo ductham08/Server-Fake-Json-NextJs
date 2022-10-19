ec9e4e63e22fea1f4f193d9fee8b8f842fec86ce
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-10-19 23:08:59 +0700

    fake api Nguyen Quang Truong

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const path = require("path");
-const cors = require('cors');
+const cors = require("cors")
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 // const middlewares = jsonServer.defaults({

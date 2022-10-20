8188dff80b0cbf7d22fb90f49da5740edd6325ff
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-10-20 23:10:23 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const middlewares = jsonServer.defaults({
 });
 server.db = router.db;
 
+
 const rules = auth.rewriter({
     // Permission rules
     // users: 600,
@@ -35,45 +36,3 @@ server.use("/api", router);
 server.listen(8080, () => {
     console.log("JSON Server is running");
 });
-
-
-// // Code Nguyen Quang Truong
-// const jsonServer = require('json-server')
-// const auth = require('json-server-auth')
-// const cors = require('cors')
-// const path = require('path')
-// const server = jsonServer.create()
-// const router = jsonServer.router(path.join(__dirname, 'db.json'))
-//     // const middlewares = jsonServer.defaults({
-//     //   noCors: true,
-//     // })
-// server.db = router.db
-
-// const rules = auth.rewriter({
-//     // Permission rules
-//     // users: 600,
-//     // messages: 640
-// })
-
-// // You must apply the middlewares in the following order
-// server.use(cors())
-// server.use(rules)
-// server.use(auth)
-// server.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://bee-navy.vercel.app')
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-//     res.setHeader('Access-Control-Allow-Headers', '*')
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     if (req.method === 'POST') {
-//         req.body.createAt = Date.now()
-//         req.body.updateAt = Date.now()
-//     } else if (req.method === 'PATCH') {
-//         req.body.updateAt = Date.now()
-//     }
-//     next()
-// })
-
-// server.use('/api', router)
-// server.listen(8080, () => {
-//     console.log('JSON Server is running')
-// })
